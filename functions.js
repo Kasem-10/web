295d40bf26938392b1f6ba4cfeff5b5ec8258459
Kasem-10 <alhumsikasem@gmail.com>
2025-07-15 10:33:23 +0200

    Add initial game implementation with canvas and player controls

---
+++ functions.js
@@ -0,0 +1,7 @@
+    function myFunction() {
+      document.getElementById("demo").innerHTML = "Paragraph changed.";
+    }
+
+    window.addEventListener("DOMContentLoaded", function () {
+      document.getElementById("demo2").innerHTML = (5 + 6) * 10;
+    });
